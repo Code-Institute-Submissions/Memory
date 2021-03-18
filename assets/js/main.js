4380adef6edc09b17e56a902106a86fc6b1527d6
FortyTwoFortyTwo <connorrowlanduk@gmail.com>
2021-03-18 20:02:13 +0000

    Make squares selectable when displaying is done
    All squares colored blue when displaying squares to select is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 let start = true;
+let boxToSelect = [];
+let maxBoxToSelect = 0;
+let currentBoxToSelect = 0;
 
 $(document).ready(function() {
 	
@@ -11,15 +14,50 @@ $(document).ready(function() {
 		SetColor($(this), "grey");
 		
 		start = false;
+		boxToSelect = [];	//empty
+		maxBoxToSelect = 1;
+		currentBoxToSelect = 0;
 		StartDisplay();
 	});
 });
 
 function StartDisplay()
 {
-	const random = Math.floor(Math.random() * $(".memory-select").length);
-	SetColor($(".memory-select").eq(random), "green");
+	if (currentBoxToSelect < maxBoxToSelect)
+	{
+		if (currentBoxToSelect <= boxToSelect.length)
+		{
+			//Add new square from list of already-selected squares to the list
+			const random = Math.floor(Math.random() * $(".memory-select").length);
+			boxToSelect.push(random);
+		}
+		
+		//Display square to select green
+		SetAllSelectColor("grey");
+		SetColor($(".memory-select").eq(boxToSelect[currentBoxToSelect]), "green");
+		
+		//Add delay for next square to display
+		currentBoxToSelect++;
+		setTimeout(SetAllSelectColor, 300, "grey");
+		setTimeout(StartDisplay, 400);
+	}
+	else
+	{
+		//Done displaying, allow player to select
+		currentBoxToSelect = 0;
+		SetNextSelect();
+	}
 };
+
+function SetNextSelect()
+{
+	//Make all squares selectable, and give class "memory-correct" to correct square
+	SetAllSelectColor("blue");
+	$(".memory-select").removeClass("memory-correct");
+	$(".memory-select").eq(boxToSelect[currentBoxToSelect]).addClass("memory-correct");
+	
+	currentBoxToSelect++;
+}
 
 function SetColor(btn, color)
 {
@@ -30,4 +68,15 @@ function SetColor(btn, color)
 	btn.removeClass("color-background-grey");
 	
 	btn.addClass("color-background-" + color);
+}
+
+function SetAllSelectColor(color)
+{
+	//Reset all square colors and set all square given color
+	$(".memory-select").removeClass("color-background-red");
+	$(".memory-select").removeClass("color-background-green");
+	$(".memory-select").removeClass("color-background-blue");
+	$(".memory-select").removeClass("color-background-grey");
+	
+	$(".memory-select").addClass("color-background-" + color);
 }
